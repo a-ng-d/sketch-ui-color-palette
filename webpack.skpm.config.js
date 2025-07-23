1eb6f3bdb6aef6ffb4f365ea3ba026fc8426c3aa
Aurélien Grimaud <a.grimaud@involt.io>
2025-07-23 19:24:28 +0200

    refactor: clean up webpack configuration and remove unnecessary comments

--- webpack.skpm.config.js
+++ webpack.skpm.config.js
@@ -2,14 +2,11 @@ const Dotenv = require("dotenv-webpack");
 const path = require("path");
 
 module.exports = function (config, entry) {
-  // Charger les variables d'environnement depuis .env.local
   config.plugins = config.plugins || [];
   config.plugins.push(
     new Dotenv({
       path: path.resolve(__dirname, ".env.local"),
-      systemvars: true, // Charge également les variables d'environnement système
-      safe: false, // Charge .env.example si présent (facultatif)
-      defaults: false, // Charge .env.defaults si présent (facultatif)
+      systemvars: true,
     })
   );
 
@@ -19,7 +16,6 @@ module.exports = function (config, entry) {
         setImmediate: false,
       };
 
-  // Configuration des résolutions d'alias
   config.resolve = {
     ...config.resolve,
     extensions: [".ts", ".tsx", ".js", ".jsx", ".json"],
@@ -36,10 +32,9 @@ module.exports = function (config, entry) {
     },
   };
 
-  // Configurer Babel pour prendre en charge les opérateurs modernes (comme ??)
   config.module.rules.push({
     test: /\.(js|jsx)$/,
-    exclude: /node_modules\/(?!(@a_ng_d)\/).*/,
+    exclude: /node_modules/,
     use: {
       loader: "babel-loader",
       options: {
@@ -61,7 +56,25 @@ module.exports = function (config, entry) {
     },
   });
 
-  // Loader pour TypeScript et TSX (Preact)
+  config.module.rules.push({
+    test: /\.(js|jsx)$/,
+    include: [
+      /node_modules\/@sentry/,
+      /node_modules\/@sentry-internal/,
+      /node_modules\/@a_ng_d/,
+    ],
+    use: {
+      loader: "babel-loader",
+      options: {
+        presets: [["@babel/preset-env", { targets: { node: "current" } }]],
+        plugins: [
+          "@babel/plugin-proposal-nullish-coalescing-operator",
+          "@babel/plugin-proposal-optional-chaining",
+        ],
+      },
+    },
+  });
+
   config.module.rules.push({
     test: /\.tsx?$/,
     use: [
@@ -72,7 +85,7 @@ module.exports = function (config, entry) {
           compilerOptions: {
             jsx: "react-jsx",
             jsxImportSource: "preact",
-            target: "es2019", // Cibler une version compatible avec les opérateurs ES modernes
+            target: "es2019",
           },
         },
       },
@@ -80,21 +93,19 @@ module.exports = function (config, entry) {
     exclude: /node_modules/,
   });
 
-  // Loader pour les fichiers d'images (webp, gif, png, jpg, etc.)
   config.module.rules.push({
     test: /\.(webp|gif|png|jpe?g|svg)$/i,
     use: [
       {
         loader: "url-loader",
         options: {
-          limit: Infinity, // Toujours encoder en base64 quelle que soit la taille
+          limit: Infinity,
           encoding: "base64",
         },
       },
     ],
   });
 
-  // Loader HTML
   config.module.rules.push({
     test: /\.(html)$/,
     use: [
@@ -111,7 +122,6 @@ module.exports = function (config, entry) {
     ],
   });
 
-  // Loader CSS unifié pour tous les fichiers CSS
   config.module.rules.push({
     test: /\.css$/,
     use: [
