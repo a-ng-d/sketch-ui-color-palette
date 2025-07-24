e99c5418948ea9762e4632dd81ccfd913c9f5a81
Aurélien Grimaud <a.grimaud@involt.io>
2025-07-24 15:08:53 +0200

    style: standardize quotes and formatting in webpack configuration

--- webpack.skpm.config.js
+++ webpack.skpm.config.js
@@ -1,60 +1,62 @@
-const path = require("path");
-const Dotenv = require("dotenv-webpack");
+/* eslint-disable no-undef */
+/* eslint-disable @typescript-eslint/no-var-requires */
+const path = require('path')
+const Dotenv = require('dotenv-webpack')
 
 module.exports = function (config, entry) {
-  config.plugins = config.plugins || [];
+  config.plugins = config.plugins || []
   config.plugins.push(
     new Dotenv({
-      path: path.resolve(__dirname, ".env.local"),
+      path: path.resolve(__dirname, '.env.local'),
       systemvars: true,
     })
-  );
+  )
 
   config.node = entry.isPluginCommand
     ? false
     : {
         setImmediate: false,
-      };
+      }
 
   config.resolve = {
     ...config.resolve,
-    extensions: [".ts", ".tsx", ".js", ".jsx", ".json"],
+    extensions: ['.ts', '.tsx', '.js', '.jsx', '.json'],
     alias: {
       ...config.resolve.alias,
-      react: "preact/compat",
-      "react-dom/test-utils": "preact/test-utils",
-      "react-dom": "preact/compat",
-      "react/jsx-runtime": "preact/jsx-runtime",
-      "@ui-lib": require("path").resolve(
+      react: 'preact/compat',
+      'react-dom/test-utils': 'preact/test-utils',
+      'react-dom': 'preact/compat',
+      'react/jsx-runtime': 'preact/jsx-runtime',
+      '@ui-lib': require('path').resolve(
         __dirname,
-        "packages/ui-ui-color-palette/src"
+        'packages/ui-ui-color-palette/src'
       ),
     },
-  };
+  }
 
   config.module.rules.push({
     test: /\.(js|jsx)$/,
     exclude: /node_modules/,
     use: {
-      loader: "babel-loader",
+      loader: 'babel-loader',
       options: {
         presets: [
-          ["@babel/preset-env", { targets: { node: "current" } }],
+          ['@babel/preset-env', { targets: { node: 'current' } }],
           [
-            "@babel/preset-react",
+            '@babel/preset-react',
             {
-              runtime: "automatic",
-              importSource: "preact",
+              runtime: 'automatic',
+              importSource: 'preact',
             },
           ],
         ],
         plugins: [
-          "@babel/plugin-proposal-nullish-coalescing-operator",
-          "@babel/plugin-proposal-optional-chaining",
+          '@babel/plugin-proposal-nullish-coalescing-operator',
+          '@babel/plugin-proposal-optional-chaining',
         ],
       },
     },
-  });
+  })
 
   config.module.rules.push({
     test: /\.(js|jsx)$/,
@@ -64,76 +66,76 @@ module.exports = function (config, entry) {
       /node_modules\/@a_ng_d/,
     ],
     use: {
-      loader: "babel-loader",
+      loader: 'babel-loader',
       options: {
-        presets: [["@babel/preset-env", { targets: { node: "current" } }]],
+        presets: [['@babel/preset-env', { targets: { node: 'current' } }]],
         plugins: [
-          "@babel/plugin-proposal-nullish-coalescing-operator",
-          "@babel/plugin-proposal-optional-chaining",
+          '@babel/plugin-proposal-nullish-coalescing-operator',
+          '@babel/plugin-proposal-optional-chaining',
         ],
       },
     },
-  });
+  })
 
   config.module.rules.push({
     test: /\.tsx?$/,
     use: [
       {
-        loader: "ts-loader",
+        loader: 'ts-loader',
         options: {
           transpileOnly: true,
           compilerOptions: {
-            jsx: "react-jsx",
-            jsxImportSource: "preact",
-            target: "es2019",
+            jsx: 'react-jsx',
+            jsxImportSource: 'preact',
+            target: 'es2019',
           },
         },
       },
     ],
     exclude: /node_modules/,
-  });
+  })
 
   config.module.rules.push({
     test: /\.(webp|gif|png|jpe?g|svg)$/i,
     use: [
       {
-        loader: "url-loader",
+        loader: 'url-loader',
         options: {
           limit: Infinity,
-          encoding: "base64",
+          encoding: 'base64',
         },
       },
     ],
-  });
+  })
 
   config.module.rules.push({
     test: /\.(html)$/,
     use: [
       {
-        loader: "@skpm/extract-loader",
+        loader: '@skpm/extract-loader',
       },
       {
-        loader: "html-loader",
+        loader: 'html-loader',
         options: {
-          attrs: ["img:src", "link:href"],
+          attrs: ['img:src', 'link:href'],
           interpolate: true,
         },
       },
     ],
-  });
+  })
 
   config.module.rules.push({
     test: /\.css$/,
     use: [
       entry.isPluginCommand
-        ? { loader: "@skpm/extract-loader" }
-        : { loader: "style-loader" },
+        ? { loader: '@skpm/extract-loader' }
+        : { loader: 'style-loader' },
       {
-        loader: "css-loader",
+        loader: 'css-loader',
         options: {
           importLoaders: 1,
         },
       },
     ],
-  });
-};
+  })
+}
