ac1c96369c3efa8cdc34428ad8d652677342f4f4
Aurélien Grimaud <a.grimaud@involt.io>
2025-09-03 00:11:41 +0200

    fix: reduce Mixpanel session recording percentage from 50 to 5

--- resources/webview.js
+++ resources/webview.js
@@ -32,7 +32,7 @@ if (globalConfig.env.isMixpanelEnabled && mixpanelToken !== undefined) {
     disable_cookie: true,
     ignore_dnt: true,
     opt_out_tracking_by_default: true,
-    record_sessions_percent: 50,
+    record_sessions_percent: 5,
     record_mask_text_selector: '*',
     record_block_selector: 'img',
     record_heatmap_data: true,
