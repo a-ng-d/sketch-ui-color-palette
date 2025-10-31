0578124c215497b129c7a7988d4d12e08963714c
Aurélien Grimaud <a.grimaud@involt.io>
2025-10-31 15:22:02 +0100

    fix: update ESLint rule for no-explicit-any and clean up type declarations in env.d.ts

--- env.d.ts
+++ env.d.ts
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
 declare module '*.webp' {
   const value: string
   export = value
@@ -15,42 +17,16 @@ declare module 'jszip'
 declare module 'react-dom/client'
 declare module 'apca-w3'
 declare module 'sketch/settings' {
-  export default {
-    settingForKey: (key: string) => any,
-    setSettingForKey: (key: string, value: any) => any,
-    documentSettingForKey: (document: any, key: string) => any,
-    setDocumentSettingForKey: (document: any, key: string, value: any) => any,
-    layerSettingForKey: (layer: any, key: string) => any,
-    setLayerSettingForKey: (layer: any, key: string, value: any) => any,
-  };
+  const sketchSettings: any
+  export default sketchSettings
 }
-declare module "sketch/dom" {
-  export default {
-    StackLayout: any,
-    Direction: any,
-    AlignItems: any,
-    JustifyContent: any,
-    Group: any,
-    getSelectedDocument: () => any,
-    Artboard: any,
-    Rectangle: any,
-    FlexLayout: any,
-    SharedStyle: any,
-    FillType: any,
-    Style: any,
-    FlexSizing: any,
-    GroupBehavior: any,
-    Text: any,
-    ShapePath: any,
-    Image: any,
-    Sketch: any,
-    createLayerFromData: (data: string, type: string) => any,
-  }
+
+declare module 'sketch/dom' {
+  const sketchDom: any
+  export default sketchDom
 }
-declare module "sketch" {
-  export default {
-    settingForKey: (key: string) => any,
-    Swatch: any,
-  };
+declare module 'sketch' {
+  const sketch: any
+  export default sketch
 }
 declare module 'mixpanel-browser'
