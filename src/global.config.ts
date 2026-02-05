be553058b8e4752e3c2b77dfa122087814d3953d
Aurélien Grimaud <a.grimaud@involt.io>
2026-02-05 12:15:33 +0100

    feat: update credits renewal period and version in global config

--- src/global.config.ts
+++ src/global.config.ts
@@ -34,8 +34,8 @@ const globalConfig: Config = {
     isCreditsEnabled: true,
     trialTime: 72,
     creditsLimit: 500,
-    creditsRenewalPeriodDays: 3,
-    creditsRenewalPeriodHours: 72,
+    creditsRenewalPeriodDays: 2,
+    creditsRenewalPeriodHours: 48,
   },
   dbs: {
     palettesDbViewName: isDev
@@ -91,7 +91,7 @@ const globalConfig: Config = {
     algorithmVersion: 'v3',
     paletteVersion: '2025.06',
     pluginVersion: process.env.npm_package_version as string,
-    creditsVersion: '2026.01',
+    creditsVersion: '2026.02',
   },
   features: doSpecificMode(
     [
